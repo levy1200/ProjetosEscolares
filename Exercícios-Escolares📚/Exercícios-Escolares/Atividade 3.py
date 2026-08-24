f10567a114637b9b0ed471ff039c539915c211d5
LEVY <joselevy1200@gmail.com>
2026-08-24 09:54:32 -0300

    Criar Atividade 3.py

---
+++ Exercícios-Escolares📚/Exercícios-Escolares/Atividade 3.py
@@ -0,0 +1,11 @@
+num1 = int(input("Digite o 1 numero: "))
+num2 = int(input("Digite o 2 numero: "))
+num3 = int(input("Digite o 3 numero: "))
+
+if num1 >= num2 and num1 >= num3:
+	print(num1)
+elif num2 >= num1 and num2 >= num3:
+	print(num2)
+elif num3 >= num1 and num3 >= num2:
+	print(num3)
+
