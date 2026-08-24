9af196c4460add712b6a5ee932d477cfdc43c9c1
LEVY <joselevy1200@gmail.com>
2026-08-24 09:57:11 -0300

    Criar Atividade 5.py
    Neste exercício fiz um programa que troca os números escolhidos

---
+++ Exercícios-Escolares📚/Exercícios-Escolares/Atividade 5.py
@@ -0,0 +1,8 @@
+n1 = int(input("Digite o primeiro numero: "))
+n2 = int(input("Digite o segundo numero: "))
+
+s = (n1)
+s1 = (n2)
+
+print(f"a troca do primeiro numero pelo segundo deu {n2}")
+print(f"a troca do segundo numero pelo primeiro deu {n1}")
