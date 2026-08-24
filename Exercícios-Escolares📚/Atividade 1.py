b6a36978b0805e21ef40bdb95e2117fbe3ef4476
LEVY <joselevy1200@gmail.com>
2026-08-24 09:48:21 -0300

    Criar Atividade 1.py

---
+++ Exercícios-Escolares📚/Atividade 1.py
@@ -0,0 +1,18 @@
+S = input("Digite uma letra: ").lower()
+
+if S == "a":
+	print("é vogal")
+
+elif S == "e":
+	print("é vogal")
+	
+elif S == "i":
+	print("é vogal")
+	
+elif S == "o":
+	print("é vogal")
+	
+elif S == "u":
+	print("é vogal")
+else:
+	print("é consoante")
