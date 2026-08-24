5097d116bb9f30e4f0fbb8dfd6713ac3361d98f4
LEVY <joselevy1200@gmail.com>
2026-08-24 13:04:58 -0300

    Excluir Atividade 5.py

--- Atividade 5.py
+++
@@ -1,8 +0,0 @@
-n1 = int(input("Digite o primeiro numero: "))
-n2 = int(input("Digite o segundo numero: "))
-
-s = (n1)
-s1 = (n2)
-
-print(f"a troca do primeiro numero pelo segundo deu {n2}")
-print(f"a troca do segundo numero pelo primeiro deu {n1}")
